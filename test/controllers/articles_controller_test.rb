6f948687afb2acb22acdd2467652d1bc6f2136aa
Olivier-Kango <olivierkango@gmail.com>
2023-05-20 23:15:37 +0300

    Add new input

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -17,7 +17,7 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create article" do
     assert_difference("Article.count") do
-      post articles_url, params: { article: { body: @article.body, published_at: @article.published_at, title: @article.title } }
+      post articles_url, params: { article: { body: @article.body, excerpt: @article.excerpt, location: @article.location, published_at: @article.published_at, title: @article.title } }
     end
 
     assert_redirected_to article_url(Article.last)
@@ -34,7 +34,7 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update article" do
-    patch article_url(@article), params: { article: { body: @article.body, published_at: @article.published_at, title: @article.title } }
+    patch article_url(@article), params: { article: { body: @article.body, excerpt: @article.excerpt, location: @article.location, published_at: @article.published_at, title: @article.title } }
     assert_redirected_to article_url(@article)
   end
 
