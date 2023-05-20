85db0f10790b7f9872843eb76c9327eea8fea95b
Olivier-Kango <olivierkango@gmail.com>
2023-05-20 23:03:42 +0300

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_20_194451) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_195935) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "published_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "excerpt"
+    t.string "location"
   end
 
 end
