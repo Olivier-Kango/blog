a8ac9c7f41bf511002fe1e6306a6455c3849162a
Olivier-Kango <olivierkango@gmail.com>
2024-01-11 14:59:27 +0200

    Destroy model Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_11_110743) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "published_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
