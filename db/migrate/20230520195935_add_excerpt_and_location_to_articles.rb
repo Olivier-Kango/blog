939206d1b1fa4297385f85a77d2f7abb573c23f3
Olivier-Kango <olivierkango@gmail.com>
2024-01-11 10:46:36 +0200

    Delete content

--- db/migrate/20230520195935_add_excerpt_and_location_to_articles.rb
+++
@@ -1,6 +0,0 @@
-class AddExcerptAndLocationToArticles < ActiveRecord::Migration[7.0]
-  def change
-    add_column :articles, :excerpt, :string
-    add_column :articles, :location, :string
-  end
-end
